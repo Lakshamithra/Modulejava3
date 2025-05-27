e6c37c375a13c5fa8e2f523be47727a0203ecfcd
Lakshamithra <143053825+Lakshamithra@users.noreply.github.com>
2025-05-27 21:42:45 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,15 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int intVar = 10;
+        float floatVar = 10.5f;
+        double doubleVar = 20.99;
+        char charVar = 'A';
+        boolean boolVar = true;
+
+        System.out.println("Integer: " + intVar);
+        System.out.println("Float: " + floatVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("Boolean: " + boolVar);
+    }
+}
